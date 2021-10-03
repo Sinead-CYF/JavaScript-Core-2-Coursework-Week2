eb98ec89453c4aa8e3d6c9e46c085f9f3a44880b
Sinead <77743497+Sinead-CYF@users.noreply.github.com>
2021-10-04 00:40:46 +0100

    Exercise E part complete
    Unsure how to change the colours of the selected option and also unsure what the paragraph is for and where it should be.

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -1,7 +1,60 @@
 function listOfColours(colours) {
-  // Write your code here...
+  const content = document.querySelector("#content");
+  const heading = document.createElement("h1");
+  heading.textContent = "List of Colours";
+  content.appendChild(heading);
+  let selectList = document.createElement("select");
+  content.appendChild(selectList);
+  // const optionName = document.createElement("option");
+  // selectList.appendChild(optionName);
+  // optionName.innerText("Pick a Colour");
+
+  for (let i = 0; i < colours.length; i++) {
+    let optionEl = document.createElement("option");
+    selectList.appendChild(optionEl);
+    optionEl.innerText = colours[i];
+  }
+
+  if (optionEl[i].selected.value === "red"){ 
+   optionEl.style.color = "red";
+  }
 }
 
+//Can't figure out how to change the colours upon selection. 
+
+// if (optionEl.selected && optionEl.innerText === "red"){
+//   optionEl.style.color = "red";
+// }
+
+// .selectedIndex = "2";
+
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
+colours.unshift("Choose a colour");
 
 listOfColours(colours);
+
+/*
+# List of colours
+
+The function **listOfColours** takes an array of colour strings:
+
+We want to render a dropdown list of colours on the page. DONE. 
+
+When a colour is selected, 
+the selection will appear on screen e.g. 'You have selected: blue'.
+The text informing the user of their selection will also change colour 
+to match the selected colour e.g. 
+'You have selected: blue' will display all in blue. 
+'You have selected: green' will be green etc.
+
+## Exercise
+
+* Create a `<select>` element.
+* Create a `<p>` element.
+* Iterate through the array of colours.
+* Each element in the array should add a `<option>` to the `<select>`, 
+  where the `<option>` is the element in the array.
+* Each `<option>` should have a 'click' event listener to update the contents and colour of the `<p>` with the selected colour.
+* All of your HTML should go inside the `<div>` with the id **"content"**.
+
+*/
